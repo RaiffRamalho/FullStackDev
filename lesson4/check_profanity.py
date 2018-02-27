fa4c6226f571b93fd7054c87c3fb5683dad7e6c4
raiff <rmello@lenovo.com>
2018-02-27 17:31:55 -0300

    urllib.request for python 3.x

--- lesson4/check_profanity.py
+++ lesson4/check_profanity.py
@@ -1,16 +1,16 @@
-import urllib
+import urllib.request
 
 def read_text():
 	quotes = open(r"C:\Users\raiff\Desktop\fullStack\lesson4\movie_quotes.txt")
 	content = quotes.read()
-	print(content)
 	quotes.close()
 	check_profanity(content)
 	
 def check_profanity(text):
-	connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text)
+	connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q=shot")
 	output = connection.read()
 	print(output)
-	connection.close
+	connection.close()
+	
 	
 read_text()
