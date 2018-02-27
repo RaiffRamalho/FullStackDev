a6b63893224fb8af6beca98b05720c25a2c8b482
raiff <rmello@lenovo.com>
2018-02-27 16:52:09 -0300

    sending text via twilio ok

--- lesson3/textLesson.py
+++ lesson3/textLesson.py
@@ -4,13 +4,10 @@ account_sid = "AC2a8df85472c477759f574af1d68aa08d"
 auth_token = "1cb436df7d064942f38a77b466bd2e6a"
 client = Client(account_sid, auth_token)
 
-call = client.calls.create(
+message = client.messages.create(
+	body="Let's grab lunch at Milliways tomorrow!",
     to="+5583999691844",
     from_="+15402355704",
-    url="http://demo.twilio.com/docs/voice.xml"
 )
 
-call = client.calls.get("CA42ed11f93dc08b952027ffbc406d0868")
-print(call.to)
-
-print(call.sid)
+print(message.sid)
