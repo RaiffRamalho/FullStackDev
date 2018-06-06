947aab93e06eafee51725bbfce8afbacc1e96eca
raiff <rmello@lenovo.com>
2018-06-06 11:36:27 -0300

    starting fixes

--- vagrant/FSND-Virtual-Machine/vagrant/README.md
+++ vagrant/FSND-Virtual-Machine/vagrant/README.md
@@ -2,14 +2,23 @@
 
 This is the modules 3  first project of udacity course of Fullstack Developer
 
-### Prerequisites
+## Prerequisites
 
 Python 2.7 installed in the machine and the set up described in the project specification
 
-### How to run
+## How to run
 
 The file logProj.py need to be inside the vagrant folder.
 
 ```
 python logProj.py
 ```
+
+### Views created
+
+* articlesPopular with columns (title, mostPop)
+* authorPopularNum with columns (author, totalViews)
+* errors with column (time)
+* success with column (time)
+* errorsCounted with columns (time, countin)
+* successCounted with columns (time, countin)
--- vagrant/FSND-Virtual-Machine/vagrant/logProj.py
+++ vagrant/FSND-Virtual-Machine/vagrant/logProj.py
@@ -1,27 +1,6 @@
 import psycopg2
 db = psycopg2.connect("dbname=news")
 
-# authors = db.cursor()
-# authors.execute("select name, id from authors;")    
-# var = authors.fetchall()
-# for element in var:
-#     print element
-
-# print ("---------------------------")
-# articles = db.cursor()
-# articles.execute("select author, title from articles;")    
-# var2 = articles.fetchall()
-# for element in var2:
-#     print element
-
-# print ("---------------------------")
-# logs = db.cursor()
-# logs.execute("select * from log limit 12;")    
-# var3 = logs.fetchall()
-# for element in var3:
-#     print element
-# print ("---------------------------")
-
 print ("QUESTION1")
 print ("---------------------------")
 
@@ -30,7 +9,7 @@ quest1.execute("select title , count(*) from articles, log where log.path "+
 "like '%'||articles.slug group by title order by count desc limit 3;")
 var4 = quest1.fetchall()
 for element in var4:
-    print element
+    print (element[0] + " - " + str(element[1]))
 
 print ("---------------------------")
 print ("QUESTION 2")
@@ -54,7 +33,7 @@ quest2.execute("select name, totalViews from authors, authorPopularNum "+
 "where authors.id = authorPopularNum.author order by totalViews desc;")
 var6 = quest2.fetchall()
 for element in var6:
-    print element
+    print (element[0] + " - " + str(element[1]))
 
 print ("---------------------------")
 print ("QUESTION 3")
@@ -81,7 +60,7 @@ logs.execute("select successCounted.time, successCounted.countin, errorsCounted.
 var10 = logs.fetchall()
 for element in var10:
     if element[2] > (element[1] * 0.1):
-        print element
+        print (element)
 print ("---------------------------")
 
 
